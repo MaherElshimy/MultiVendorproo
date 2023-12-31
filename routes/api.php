d0a75632537fce683d75200ed9bc0df706c40b92
Maher Elshimy <maher.elshimy22@gmail.com>
2023-12-31 10:42:58 +0200

    This is After MultiAuth

--- routes/api.php
+++ routes/api.php
@@ -21,36 +21,19 @@ use App\Http\Controllers\VendorController;
 |
 */
 
-// //// Only for users
-// Route::middleware(['auth:sanctum', 'type.user'])->group(function () {
-//     Route::get('/users/orders', [OrderController::class, 'orders']);
-// });
-
-/*
- * ////////// Authentication routes ///////////////////////
- */
-
-
 
 // Only for admins
 Route::middleware(['auth:admins'])->group(function () {
-    // Create a new category
     Route::post('/categories', [CategoryController::class, 'store']);
-    // Update a specific category by ID
     Route::put('/categories/{id}', [CategoryController::class, 'update']);
-    // Delete a specific category by ID
     Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
 });
 
 // Only for vendors
 Route::middleware(['auth:vendors'])->group(function () {
-    // Delete the duplicated category routes from here
-    // POST: Create a new product
     Route::post('/products', [ProductController::class, 'create']);
-    // PUT/PATCH: Update a specific product by ID
     Route::put('/products/{id}', [ProductController::class, 'update']);
     Route::patch('/products/{id}', [ProductController::class, 'update']);
-    // DELETE: Delete a specific product by ID
     Route::delete('/products/{id}', [ProductController::class, 'destroy']);
 });
 
@@ -74,52 +57,19 @@ Route::post('/logout', [AuthController::class, 'logout']);
 
 
 
-/*
- * /////////////////////////////////////////////////////////
- * /////////////////// Public Routes ///////////////////////
- * ////////////////////////////////////////////////////////
- */
-
-
-
-/*//////////////////////////////////////////////////////////
- * //////////////// Special Catagory ///////////////////////
- */
-
-// Show all categories
 Route::get('/categories', [CategoryController::class, 'index']);
-
-// Show a specific category by ID
 Route::get('/categories/{id}', [CategoryController::class, 'show']);
 
 
-
-/*//////////////////////////////////////////////////////////
- * //////////////// Special Products ///////////////////////
- */
-// GET: Retrieve all products
 Route::get('/products', [ProductController::class, 'index']);
-
-// GET: Retrieve a specific product by ID
 Route::get('/products/{id}', [ProductController::class, 'show']);
-
-// Search : Search a specific product by name
 Route::get('/products/search/{name}', [ProductController::class, 'searchByName']);
 
 
-/*
- * /////////////////////////////////////////////////////////
- * //////////////// Protected Routes ///////////////////////
- * ////////////////////////////////////////////////////////
- */
 Route::group(['middleware' => ['auth:sanctum']], function() {
-
-
-    // WishLists Is Here.
     Route::post('/wishlist/add/{productId}', [WishlistController::class, 'addToWishlist']);
     Route::delete('/wishlist/remove/{productId}', [WishlistController::class, 'removeFromWishlist']);
 
-    // Orders Is Here.
     Route::post('/orders/place', [OrderController::class, 'placeOrder']);
     Route::get('/orders/{id}', [OrderController::class, 'getOrderDetails']);
     Route::post('/orders/{id}/cancel', [OrderController::class, 'cancelOrder']);
